e087d81996650cc98077f10c43cffe411fbb7205
Martin Polden <martin.polden@gmail.com>
2016-05-16 18:03:15 +0200

    Display date in UTC

--- cmd/search.go
+++ cmd/search.go
@@ -20,7 +20,7 @@ func (c *Search) WriteTable(dirs []database.Dir, w io.Writer) {
 	table := tablewriter.NewWriter(w)
 	table.SetHeader([]string{"Site", "Path", "Date"})
 	for _, d := range dirs {
-		date := time.Unix(d.Modified, 0).Format("2006-01-02 15:04:05 MST")
+		date := time.Unix(d.Modified, 0).UTC().Format("2006-01-02 15:04:05 MST")
 		row := []string{d.Site, d.Path, date}
 		table.Append(row)
 	}
